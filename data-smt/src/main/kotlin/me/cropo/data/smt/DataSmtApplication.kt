3d6e6556402d351c15a7f5cc99f068d97ab4170f
danny <hoon2585@gmail.com>
2023-03-29 18:23:34 +0900

    fix - AutoConfig 삭제

--- data-smt/src/main/kotlin/me/cropo/data/smt/DataSmtApplication.kt
+++ data-smt/src/main/kotlin/me/cropo/data/smt/DataSmtApplication.kt
@@ -2,8 +2,10 @@ package me.cropo.data.smt
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@EnableJpaRepositories
 class DataSmtApplication
 
 fun main(args: Array<String>) {
